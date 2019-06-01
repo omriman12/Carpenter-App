a2504eba3efc3795ac2a0c1abbb233288827ffb5
ömriman12 <mriby@gmail.com>
2019-06-01 13:42:26 +0300

    minor

--- Carpenter/app/src/AppRouter.js
+++ Carpenter/app/src/AppRouter.js
@@ -2,16 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import AppUrls from './model/constants/AppUrls';
 import Home from './views/home/Home';
+import AssetsSearch from './views/assetsSearch/AssetsSearch';
 
 const AppRouter = () => (
     <Switch>
       <Route exact path='/' component={Home}/>
       <Route path={AppUrls.Home.Root} component={Home}/>
-      {/* <Route path={AppUrls.Assets.Root} component={AssetsSearch}/>
-      <Route path={AppUrls.About.Root} component={About}/>
-      <Route path={AppUrls.Contact.Root} component={Contact}/>
-      <Route path={AppUrls.Users.Root} component={Users}/>`
-      <Route path={AppUrls.Login.Root} component={SignIn}/> */}
+      <Route path={AppUrls.Assets.Root} component={AssetsSearch}/>
     </Switch>
 );
 
--- Carpenter/app/src/views/assetsSearch/AssetsSearch.js
+++ Carpenter/app/src/views/assetsSearch/AssetsSearch.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import DataTable from 'react-data-table-component';
+import { withStyles  } from '@material-ui/core/styles';
+
 
 //components
 import AppContentLayout from '../../Layouts/AppContentLayout';
 import AppTitleLayout from '../../Layouts/AppTitleLayout';
 
+const data = [{ id: 1, wood: 'wood 1', type: 'wood type 1', year: '1982' }];
+const columns = [
+    {
+        name: 'Wood',
+        selector: 'wood',
+        sortable: true,
+    },
+    {
+        name: 'Type',
+        selector: 'type',
+        sortable: true,
+    },
+    {
+        name: 'Year',
+        selector: 'year',
+        sortable: true,
+        // right: true,
+    },
+];
+
+
+const styles = theme => ({
+    tableWrapper: {
+        width: '50%',
+        margin: '0 auto',
+    },
+    
+});
+
 class AssetsSearch extends Component {
     render() {
+        let classes = this.props.classes;
         return (
             <React.Fragment>
                 <AppTitleLayout>
                     חיפוש נכסים
                 </AppTitleLayout>
                 <AppContentLayout>
-                    תוכן
-s                </AppContentLayout>
+                    <div className={classes.tableWrapper}>
+                        <DataTable
+                            columns={columns}
+                            data={data}
+                        />
+                    </div>
+                
+                </AppContentLayout>
             </React.Fragment>
         );
     }
 }
-
-export default AssetsSearch;
+AssetsSearch.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+  
+export default withStyles(styles)(AssetsSearch);
